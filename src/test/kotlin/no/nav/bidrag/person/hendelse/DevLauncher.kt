64dabc18ec2684655b30af8a20f96914992297f5
s148719 <jan.kjetil.skar@nav.no>
2022-08-05 13:48:26 +0200

    diverse

--- src/test/kotlin/no/nav/bidrag/person/hendelse/DevLauncher.kt
+++ src/test/kotlin/no/nav/bidrag/person/hendelse/DevLauncher.kt
@@ -1,11 +1,10 @@
 package no.nav.bidrag.person.hendelse
 
 import no.nav.bidrag.person.hendelse.konfigurasjon.ApplicationConfig
-import no.nav.security.token.support.test.spring.TokenGeneratorConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Import
 
-@Import(TokenGeneratorConfiguration::class, ApplicationConfig::class)
+@Import(ApplicationConfig::class)
 class DevLauncher
 
 fun main(args: Array<String>) {
