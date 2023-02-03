a5840de8e994499b19f2d79db46974802403cc18
s148719 <jan.kjetil.skar@nav.no>
2023-02-03 15:58:12 +0100

    Gjenopprette shedlock-tabellen

--- src/main/resources/db/migration/V1_0_2__create-table-shedlock.sql
+++ src/main/resources/db/migration/V1_0_2__create-table-shedlock.sql
@@ -4,8 +4,8 @@
 CREATE TABLE shedlock
 (
     name VARCHAR(64),
-    lock_until TIMESTAMP(3) NULL,
-    locked_at TIMESTAMP(3) NULL,
+    lock_until TIMESTAMP NOT NULL,
+    locked_at TIMESTAMP NOT NULL,
     locked_by VARCHAR(255),
     PRIMARY KEY (name)
 )
