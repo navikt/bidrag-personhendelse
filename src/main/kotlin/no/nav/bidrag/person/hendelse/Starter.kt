e6cf984bf93f361ee10666813e8244c2dbc8707f
s148719 <jan.kjetil.skar@nav.no>
2023-04-20 13:09:02 +0200

    Update Starter.kt

--- src/main/kotlin/no/nav/bidrag/person/hendelse/Starter.kt
+++ src/main/kotlin/no/nav/bidrag/person/hendelse/Starter.kt
@@ -1,12 +1,14 @@
 package no.nav.bidrag.person.hendelse
 
+import no.nav.bidrag.person.hendelse.konfigurasjon.Applikasjonskonfig
 import no.nav.bidrag.person.hendelse.konfigurasjon.egenskaper.Wmq
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
-@EnableConfigurationProperties(Wmq::class)
+@Import(Applikasjonskonfig::class)
 open class Starter
 
 fun main(args: Array<String>) {
