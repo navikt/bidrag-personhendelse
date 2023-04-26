ed8f2b381406d4f8838c5927502b5d53e2216801
s148719 <jan.kjetil.skar@nav.no>
2023-04-26 15:16:20 +0200

    Update V1_0_7__update_hendelsemottak_set_column_aktorid.sql

--- src/main/resources/db/migration/V1_0_7__update_hendelsemottak_set_column_aktorid.sql
+++ src/main/resources/db/migration/V1_0_7__update_hendelsemottak_set_column_aktorid.sql
@@ -1,12 +1,19 @@
 /*
  Oppdatere aktor-tabellen med aktørid fra hendelsemottak,
- oppdatere hendelsemottak med fremmednøkkel til aktør
+ oppdatere hendelsemottak med fremmednøkkel til aktør.
+
+ Legger til en midlertidig hjelpekolonne i hendelsemottak for aktørid for økt ytelse.
  */
 
-insert into aktor (aktorid) select distinct substring(personidenter, '\d{13}') from  hendelsemottak;
+alter table hendelsemottak add column aktorid varchar(13);
+create index index_hendelsemottak_aktor_id on hendelsemottak(aktor_id);
+update hendelsemottak set aktorid = substring(personidenter, '\d{13}');
 
-update hendelsemottak set aktor_id =
-        (select id from aktor where aktorid in (select distinct substring(personidenter, '\d{13}') from hendelsemottak));
+insert into aktor (aktorid) select distinct aktorid from  hendelsemottak;
+
+update hendelsemottak set aktor_id = aktor.id
+from hendelsemottak hm
+inner join aktor on aktor.aktorid = hm.aktorid
 
 /*** rulle tilbake ***
 
