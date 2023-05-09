b8165cd3597d1dbdadc55e76577262ec4d24dd82
s148719 <jan.kjetil.skar@nav.no>
2023-05-09 11:17:21 +0200

    Logge personident til sikker logg dersom kall mot bidrag-person feiler

--- src/main/kotlin/no/nav/bidrag/person/hendelse/integrasjon/bidrag/person/BidragPersonklient.kt
+++ src/main/kotlin/no/nav/bidrag/person/hendelse/integrasjon/bidrag/person/BidragPersonklient.kt
@@ -3,11 +3,14 @@ package no.nav.bidrag.person.hendelse.integrasjon.bidrag.person
 import no.nav.bidrag.commons.web.client.AbstractRestClient
 import no.nav.bidrag.transport.person.HentePersonidenterRequest
 import no.nav.bidrag.transport.person.PersonidentDto
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.retry.annotation.Backoff
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
@@ -23,10 +26,18 @@ class BidragPersonklient(
 
     @Retryable(value = [Exception::class], maxAttempts = 10, backoff = Backoff(delay = 1000, multiplier = 2.0))
     fun henteAlleIdenterForPerson(personIdent: String): List<PersonidentDto>? {
-        return postForEntity(createUri(ENDEPUNKT_PERSONIDENTER), HentePersonidenterRequest(personIdent))
+        return try {
+            postForEntity(createUri(ENDEPUNKT_PERSONIDENTER), HentePersonidenterRequest(personIdent))
+        } catch (e: HttpStatusCodeException) {
+            log.warn("Kall mot bidrag-person for å hente alle registrerte personidenter for personident feilet med statuskode ${e.statusCode} og melding ${e.message}")
+            slog.warn("Kall mot bidrag-person for å hente alle registrerte personidenter for personident $personIdent feilet med statuskode ${e.statusCode} og melding ${e.message}")
+            throw e
+        }
     }
 
     companion object {
         const val ENDEPUNKT_PERSONIDENTER = "/personidenter"
+        val log: Logger = LoggerFactory.getLogger(this::class.java)
+        val slog: Logger = LoggerFactory.getLogger("secureLogger")
     }
 }
