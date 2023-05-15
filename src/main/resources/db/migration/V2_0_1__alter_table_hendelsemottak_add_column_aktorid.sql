1f5117d408923e04421e84bd71d953af22aab177
s148719 <jan.kjetil.skar@nav.no>
2023-05-15 15:04:03 +0200

    Update V2_0_1__alter_table_hendelsemottak_add_column_aktorid.sql

--- src/main/resources/db/migration/V2_0_1__alter_table_hendelsemottak_add_column_aktorid.sql
+++ src/main/resources/db/migration/V2_0_1__alter_table_hendelsemottak_add_column_aktorid.sql
@@ -1,9 +1,10 @@
 alter table hendelsemottak add column aktorid varchar(13);
-update hendelsemottak set aktorid = substring(personidenter, '\d{13}')
+update hendelsemottak set aktorid = substring(personidenter, '\d{13}');
+create index index_hendelsemottak_aktorid on hendelsemottak(aktorid);
 
 /*** rulle tilbake ***
 
-  alter table hendelsemottak drop column aktorid
+  alter table hendelsemottak drop column aktorid;
 
   delete from flyway_schema_history where version = '´2.0.1';
 
